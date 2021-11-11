59be244ea68465917dfa9e01925a95042c1c74f9
Stewart Lynch <slynch@createchsol.com>
2021-11-11 13:50:08 -0800

    Initial Commit

---
+++ iOS Concurrency/iOS_ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_ConcurrencyApp.swift
+//  iOS Concurrency
+//
+//  Created by Stewart Lynch on 2021-11-11.
+//
+
+import SwiftUI
+
+@main
+struct iOS_ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
